9d34b4f5e7cab8c14213e94bd2a032a526af86c9
Xingyue Huang <xh890@nyu.edu>
2017-12-08 19:44:33 -0500

    Modified fitter and MC; updated capstone report

--- Fitter/over_specification.py
+++ Fitter/over_specification.py
@@ -3,11 +3,12 @@ from matplotlib import pyplot as plt
 from Fitter.fitter import Fitter
 from Pricing.SABR import SABR_model
 
-def over_specification(fitter,check):
+def over_specification(fitter,check,params):
     '''
     This function plots ivols smile by Hagan Lognormal SABR with different equal constraints and the market ivols smile.
     @var fitter: a fitter object
     @var check: a dictionary returned by fitter.py, which stores the calibration results with a specific parameter fixed to specific values.
+    @var params: a list containing SABR parameters we concern here
     '''
     keys=check.keys()
     K_spread=fitter.K_spread #x label
@@ -18,7 +19,11 @@ def over_specification(fitter,check):
         results=check[key] #calibration result with specific equal constraint
         ivols=results.iloc[10,3:].values.tolist() #take 5Y tenor and 1Y expiry as an example
         ivols=[item*100 for item in ivols] #y label
-        plt.plot(K_spread,ivols,linestyle='solid',label='parameter='+str(key))
+        if len(params)==2:
+            label=params[0]+'='+str(list(key)[0])+','+params[1]+'='+str(list(key)[1])
+        elif len(params)==1:
+            label=params[0]+'='+str(key)
+        plt.plot(K_spread,ivols,linestyle='solid',label=label)
     plt.plot(K_spread,ivols_mkt,linestyle='--',color='black',label='MKT') #market ivol
     plt.title('Over-specification Analysis')
     plt.xlabel('K spreads (bps)')
@@ -36,5 +41,9 @@ def get_check(fitter,fix_par,fix_no,method='Hagan_ln'):
     check={}
     for no in fix_no:
         results=fitter.calibration(eqc=[fix_par,no],method=method)
-        check[no]=fitter.ivol_SABR(results['alpha'],results['beta'],results['rho'],results['nu'],method=method)
+        if len(fix_par)==1:
+            check[no]=fitter.ivol_SABR(results['alpha'],results['beta'],results['rho'],results['nu'],method=method)
+        else:
+            #The key of a dictionary cannot be a list
+            check[tuple(no)]=fitter.ivol_SABR(results['alpha'],results['beta'],results['rho'],results['nu'],method=method)
     return check
